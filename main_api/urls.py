4e1351c42bcfec0caae9b79587ff62a373660f9b
cps97 <colinstoutt@gmail.com>
2023-02-21 18:57:30 -0800

    update urls to be accurate

--- main_api/urls.py
+++ main_api/urls.py
@@ -1,17 +1,17 @@
 from django.urls import path
-from .views.player_views import Player, PlayerDetail
+from .views.player_views import PlayerClass, PlayerDetail
 from .views.team_views import TeamClass, TeamDetail
-from .views.game_views import Game, GameDetail
-from .views.recruit_views import Recruit, RecruitDetail
+from .views.game_views import GameClass, GameDetail
+from .views.recruit_views import RecruitClass, RecruitDetail
 
 
 urlpatterns = [
-    path('player/', Player.as_view(), name='player'),
+    path('player/', PlayerClass.as_view(), name='player'),
     path('player/<int:pk>/', PlayerDetail.as_view(), name='player_detail'),
-    path('recruit/', TeamClass.as_view(), name='recruit'),
-    path('recruit/<int:pk>/', TeamDetail.as_view(), name='recruit_detail'),
-    path('game/', TeamClass.as_view(), name='game'),
-    path('game/<int:pk>/', TeamDetail.as_view(), name='game_detail'),
+    path('recruit/', RecruitClass.as_view(), name='recruit'),
+    path('recruit/<int:pk>/', RecruitDetail.as_view(), name='recruit_detail'),
+    path('game/', GameClass.as_view(), name='game'),
+    path('game/<int:pk>/', GameDetail.as_view(), name='game_detail'),
     path('team/', TeamClass.as_view(), name='team'),
     path('team/<int:pk>/', TeamDetail.as_view(), name='team_detail')
 ]
