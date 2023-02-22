ce0a9a5996eb1efc28d15dd1be840dd71a6c297a
cps97 <colinstoutt@gmail.com>
2023-02-21 19:26:35 -0800

    add url

--- project4/urls.py
+++ project4/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     # path('auth/', include('auth_api.urls')),
     path('manager/', include('main_api.urls'))
 ]
